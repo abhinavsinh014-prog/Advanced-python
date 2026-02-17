bccda54af79cbf62b95f808b5b5c40f6fd38f607
Abhinav singh <abhinavsinh014@gmail.com>
2026-02-17 17:20:42 +0530

    added new file

--- samples.py
+++ samples.py
@@ -1,6 +1,13 @@
 class account:
-    def __init__(self,name,balance,deposit):
+    total = 15000
+    def __init__(self,name,id,deposit):
         self.name = name
-        self.balance = balance
+        self.id = id
         self.deposit = deposit
+
+    @classmethod
+    def std_balance(cls,deposit):
+        print(cls-int(deposit))
+r1 = account('suraj',456,5500)
+print(r1.std_balance(5500))
         
