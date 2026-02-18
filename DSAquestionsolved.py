d1f5ffc4d0b866276398270293aa7bde3e846d16
Abhinav singh <abhinavsinh014@gmail.com>
2026-02-18 23:32:31 +0530

    added new file

--- DSAquestionsolved.py
+++ DSAquestionsolved.py
@@ -263,16 +263,16 @@
 matrix = [[1,1,1],[1,0,1],[1,1,1]]
 print('before',matrix)
 find = False
-for i in range(0,len(matrix)-1):
+for i in range(len(matrix)):
     if matrix[i][0] == 0:
      find = True
-    for j in range(0,len(matrix)-1):
+    for j in range(len(matrix[0])):
         if matrix[i][j]==0:
             matrix[i][0]=0
             matrix[0][j]=0
 for i in range(1,len(matrix)):
-    for j in range(1,len(matrix)):
-        if matrix[i][0] or matrix[0][j]:
+    for j in range(1,len(matrix[0])):
+        if matrix[i][0]==0 or matrix[0][j]==0:
             matrix[i][j]= 0
 if matrix[0][0]==0:
     for j in range(1,len(matrix[0])):
