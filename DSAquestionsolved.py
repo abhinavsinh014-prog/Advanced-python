99d0e62156f14e7b6c66a397ae896df62d2f9c68
Abhinav singh <abhinavsinh014@gmail.com>
2026-02-19 22:41:23 +0530

    added new file

--- DSAquestionsolved.py
+++ DSAquestionsolved.py
@@ -305,29 +305,35 @@ class ListNode:
         self.val = val
         self.next = next
 
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
 class Solution:
     def addTwoNumbers(self, l1, l2):
-        
+
         dummy = ListNode(0)
         current = dummy
         carry = 0
-        
+
         while l1 or l2 or carry:
-            
+
             val1 = l1.val if l1 else 0
             val2 = l2.val if l2 else 0
-            
+
             total = val1 + val2 + carry
             carry = total // 10
-            
+
             current.next = ListNode(total % 10)
             current = current.next
-            
+
             if l1:
                 l1 = l1.next
             if l2:
                 l2 = l2.next
-                
-        return dummy.next
+
+        return dummy
 on = Solution()
-print(on.addTwoNumbers([1,5,6],[5,6,2]))
+print(on.addTwoNumbers([1,5,6],[5,7,1]))
