5731616671d701f8ba2b0bf146b91ff4a3090a4a
Abhinav singh <abhinavsinh014@gmail.com>
2026-02-21 19:56:48 +0530

    added new file

--- DSAquestionsolved.py
+++ DSAquestionsolved.py
@@ -366,5 +366,8 @@ class solution:
         for i in range(0,n):
             for j in range(i+1,n):
                 if arr[i]>arr[j]:
-                    return (arr[i],arr[j])
-        return (arr[i],arr[j])
+                    ans = (arr[i],arr[j])
+        return ans
+    
+obj = solution()
+print(obj.inversion([2, 4, 1, 3, 5]))
