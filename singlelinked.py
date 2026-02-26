d2faf66d841fc894be1fb5030d6bcf81e0d8e39f
Abhinav singh <abhinavsinh014@gmail.com>
2026-02-26 21:24:42 +0530

    added new file

--- singlelinked.py
+++ singlelinked.py
@@ -1,5 +1,12 @@
 class node:
-    def lsit(self,item=None,next=None):
+    def __init__(self,item=None,next=None):
         self.item = item
         self.next = next
-        
+class SLL:
+    def __init__(self,start=None):
+        self.start = start
+
+    def is_empty(self):
+        return self.start==None
+    def start(self,data):
+        n=node(data,self.start)
