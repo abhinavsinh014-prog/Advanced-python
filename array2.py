05c8a8232cefcb29ad588b4ec454f61c5c459e41
Abhinav singh <abhinavsinh014@gmail.com>
2026-03-03 12:19:23 +0530

    added new file

--- array2.py
+++ array2.py
@@ -1,12 +1,24 @@
-from numpy import *
+# from numpy import *
 
-list_1 = [1, 2, 3, 4]
-list_2 = [5, 6, 7, 8]
-list_3 = [9, 10, 11, 12]
+# list_1 = [1, 2, 3, 4]
+# list_2 = [5, 6, 7, 8]
+# list_3 = [9, 10, 11, 12]
 
-max = array([list_1, 
-                    list_2,
-                         list_3])
+# max = array([list_1, 
+#                     list_2,
+#                          list_3])
 # print("Numpy multi dimensional array in python\n",
 #       max)
-print('array shape  :',max.shape)
+# print('array shape  :',max.shape)
+
+# var = "john cena"
+
+# bash = fromiter(var,dtype='U2')
+# print("fromiter",bash)
+
+import numpy as np
+
+nam = np.arange(1, 20 , 2, 
+          dtype = np.int16)
+
+print(nam)
