93d2686557a955fad8f3449e6bb095ac78b7f5ea
Abhinav singh <abhinavsinh014@gmail.com>
2026-03-05 20:55:45 +0530

    added

--- scorebarid.py
+++ scorebarid.py
@@ -1,3 +1,23 @@
-team_A =  ["player_1","player_2","player_3","player_4","player_5","player_6","player_7","player_8","player_8","player_9","player_10","player_11"] 
+team_A =  {"player_1":0,
+           "player_2":0,
+           "player_3":0,
+           "player_4":0,
+           "player_5":0,
+           "player_6":0,
+           "player_7":0,
+           "player_8":0,
+           "player_9":0
+           ,"player_10":0,
+           "player_11":0}
 
-team_A =  ["player_1","player_2","player_3","player_4","player_5","player_6","player_7","player_8","player_8","player_9","player_10","player_11"] 
+team_B =  {"player_1":0,
+           "player_2":0,
+           "player_3":0,
+           "player_4":0,
+           "player_5":0,
+           "player_6":0,
+           "player_7":0,
+           "player_8":0,
+           "player_9":0
+           ,"player_10":0,
+           "player_11":0} 
