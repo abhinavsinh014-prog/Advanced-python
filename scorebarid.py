0203bcdf8548aed6c2c9cb6c032f9050bf63fc7c
Abhinav singh <abhinavsinh014@gmail.com>
2026-03-06 21:28:26 +0530

    Uncompletd

--- scorebarid.py
+++ scorebarid.py
@@ -40,7 +40,7 @@ winner_choice = input("bating or bowling :")
 if winner_choice == 'bating' :
     for overs in range(0,n) :
         for balls in range(1,7) :
-            run = int(input())
-            print(f"Over:{overs}.{balls}") 
+            print(f"Over:{overs}.{balls}")
+            run = int(input()) 
             total_scoe += run
-        print(f"Score : {total_scoe}  Over : {overs}")
+        print(f"Score : {total_scoe}  Over : {overs+1}")
