98b0abea91237cfd31133d7b5b936e7b93ff6250
Abhinav singh <abhinavsinh014@gmail.com>
2026-03-06 21:48:59 +0530

    adding innnings

--- scorebarid.py
+++ scorebarid.py
@@ -5,7 +5,7 @@ teamB_score = 0
 
 n = int(input("enter number of overs :"))
 total_scoe = 0
-
+wickets = 0
 team_A =  {"player_1":0,
            "player_2":0,
            "player_3":0,
@@ -36,14 +36,21 @@ winner = random.choice(["teamA","teamB"])
 print('toss winner is ',winner)
 
 winner_choice = input("bating or bowling :")
-
-for overs in range(0,n) :
-    for balls in range(1,7) :
-        print(f"Over:{overs}.{balls}")
-        run = int(input())
-
-
-        total_scoe += run
-    print(f"Score : {total_scoe}  Over : {overs+1}")
+for inning in range(1,3):
+    if inning == 1:
+      print(f"{"1st inning"}")
+    else:
+        print(f"{"2nd inning"}")
+    for overs in range(0,n) :
+        for balls in range(1,7) :
+            print(f"Over:{overs}.{balls}")
+            run = int(input())
+            if run == -1:
+                wickets += 1
+                if wickets==11:
+                    break
+            else:
+                total_scoe += run
+    print(f"Score : {total_scoe}  Over : {overs+1} wickets : {wickets}")
 
 
