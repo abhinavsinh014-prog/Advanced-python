b0b619fba970c2c290b2c2d878dfc41ffe59ac36
Abhinav singh <abhinavsinh014@gmail.com>
2026-03-07 22:34:34 +0530

    added

--- scorebarid.py
+++ scorebarid.py
@@ -37,6 +37,14 @@ winner = random.choice(["teamA","teamB"])
 print('toss winner is ',winner)
 
 winner_choice = input("bating or bowling :")
+
+if winner_choice == 'bating' :
+    print(f"{winner} choose to bating first ")
+elif winner_choice == 'bowling' :
+    print(f"{winner} choose to feilding first ")
+
+print("Let's Start the Game")
+
 for inning in range(1,3):
     if inning == 1:
       print(f"{"1st inning"}")
@@ -71,5 +79,3 @@ if total_score_1st<total_score_2nd:
     print("Team 2 is winner")
 else:
     print("Team 1 is winner")
-
-
