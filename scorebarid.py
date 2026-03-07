818ea6b2603fc94351fb03fa6ca61c12e3ae74b6
Abhinav singh <abhinavsinh014@gmail.com>
2026-03-07 22:27:21 +0530

    added

--- scorebarid.py
+++ scorebarid.py
@@ -4,7 +4,8 @@ n = int(input("enter number of overs :"))
 
 total_score_1st = 0
 total_score_2nd = 0
-wickets = 0
+wickets_1st = 0
+wickets_2nd = 0
 
 team_A =  {"player_1":0,
            "player_2":0,
@@ -44,12 +45,12 @@ for inning in range(1,3):
             print(f"Over:{overs}.{balls}")
             run = int(input())
             if run == -1:
-                wickets += 1
-                if wickets==11:
+                wickets_1st += 1
+                if wickets_1st==11:
                     break
             else:
                 total_score_1st += run
-        print(f"Score : {total_score_1st}  Over : {overs+1} wickets : {wickets}")
+        print(f"Score : {total_score_1st}  Over : {overs+1} wickets : {wickets_1st}")
     else:
         print(f"{"2nd inning"}")
         for overs in range(0,n) :
@@ -57,14 +58,14 @@ for inning in range(1,3):
                 print(f"Over:{overs}.{balls}")
                 run = int(input())
                 if run == -1:
-                    wickets += 1
-                    if wickets==11:
+                    wickets_2nd+= 1
+                    if wickets_2nd==11:
                         break
                 else:
                     total_score_2nd += run
                     if total_score_1st<total_score_2nd :
                         break
-        print(f"Score : {total_score_2nd}  Over : {overs+1} wickets : {wickets}")
+        print(f"Score : {total_score_2nd}  Over : {overs+1} wickets : {wickets_2nd}")
 
 if total_score_1st<total_score_2nd:
     print("Team 2 is winner")
