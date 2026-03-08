96b9ae1ef3b677a360002b5a5bf54656a6babea9
Abhinav singh <abhinavsinh014@gmail.com>
2026-03-08 20:47:43 +0530

    added

--- tuple03.py
+++ tuple03.py
@@ -1,6 +1,9 @@
+# adding values by using list function
 tupel = (117,49,254,94,157,83,82)
 typlr = list(tupel)
 typlr.append(160)
 print(typlr,type(typlr))
 tupel = tuple(typlr)
+print(tupel,type(tupel))
+tupel = len(typlr)
 print(tupel,type(tupel))
