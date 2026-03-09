e24c7f37f5dc9b54cdb8c9e5abd5187b65348c8b
Abhinav singh <abhinavsinh014@gmail.com>
2026-03-09 22:22:58 +0530

    Using Pandas Series and Dataframe

--- panda.py
+++ panda.py
@@ -1,7 +1,14 @@
 import pandas as pd
 import numpy as np
 
+#Pandas Series
 data = np.array(["A",'u','r','a'])
 
 s = pd.Series(data)
-print("Aura Farmer\n",s)
+print(s)
+
+#Pandas DataFrame
+lst = ['Geeks', 'For', 'Geeks', 'is', 'portal', 'for', 'Geeks']
+
+s = pd.DataFrame(lst)
+print(s)
