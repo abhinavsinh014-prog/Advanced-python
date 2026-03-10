bce57aafd8e10403319ec9b3e3723d60bc2b2d7f
Abhinav singh <abhinavsinh014@gmail.com>
2026-03-10 09:20:32 +0530

    labled-based Indexing

--- panda2.py
+++ panda2.py
@@ -5,6 +5,10 @@ data = [7,5,6,4,3,2]         #Creating a Pandas Series
 ser = pd.Series(data)
 print(ser)
 
-dat = ['A','U','R','A','F','A','R','M','E','R']
+dat = ['A','U','R','A','F','A','R','M','E','R']    #Position-based Indexing
 sen = pd.Series(dat)
 print(sen[:4])
+
+datm = ['A','U','R','A','F','A','R','M','E','R']    #labled-based Indexing
+send = pd.Series(datm,index=[30,31,32,33,34,35,36,37,38,39])
+print(send[34])
