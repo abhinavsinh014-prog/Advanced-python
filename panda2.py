da4ba5a087ee27ecc838fdabfd5e923e4917ea0a
Abhinav singh <abhinavsinh014@gmail.com>
2026-03-10 09:16:32 +0530

    Position-based Indexing

--- panda2.py
+++ panda2.py
@@ -1,6 +1,10 @@
 import pandas as pd
 
-data = [7,5,6,4,3,2]
+data = [7,5,6,4,3,2]         #Creating a Pandas Series
 
 ser = pd.Series(data)
 print(ser)
+
+dat = ['A','U','R','A','F','A','R','M','E','R']
+sen = pd.Series(dat)
+print(sen[:4])
