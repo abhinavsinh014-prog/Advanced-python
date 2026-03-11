e44a1368517f38e7f3820ec63d91156985f98723
Abhinav singh <abhinavsinh014@gmail.com>
2026-03-11 09:36:42 +0530

    using sum,subs,mult,div method

--- panda2.py
+++ panda2.py
@@ -15,8 +15,27 @@ import pandas as pd
 
 df = pd.read_csv("wwe.csv")
 
-serr = pd.Series(df['Name'])
+serr = pd.Series(df['Name'])                         
 data = serr.head(12)
 print(data)
 
-print(data.loc[0:3])
+print(data.loc[0:3])                             #Indexing a Series using .loc[]
+print(data.iloc[0:4])                            #Indexing a Series using .iloc[]
+
+
+#Binary Operations on Pandas Series
+
+df1 = pd.Series([1,2,3],index=['A','B','C'])
+df2 = pd.Series([4,5,66],index=['A','B','C'])
+
+df_sum = df1.add(df2)
+print(df_sum)
+print()
+df_sub = df2.sub(df1)
+print(df_sub)
+print()
+df_mult = df2.mul(df1)
+print(df_mult)
+print()
+df_div = df1.div(df2)
+print(df_div)
