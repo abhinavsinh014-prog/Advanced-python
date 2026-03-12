68993370e917855e970c82f088b728b5690b857c
Abhinav singh <abhinavsinh014@gmail.com>
2026-03-12 22:28:14 +0530

    Iterating Over Rows

--- pandaDataframe.py
+++ pandaDataframe.py
@@ -24,31 +24,41 @@ import pandas as pd
  
 # print(gf[['Name', 'Qualification']])
 
-data = pd.read_csv("nba.csv",index_col="Name")
-first = data.loc["Avery Bradley"]   #row selection using iloc
-second = data.loc["Tyus Jones"]
+# data = pd.read_csv("nba.csv",index_col="Name")
+# first = data.loc["Avery Bradley"]   #row selection using iloc
+# second = data.loc["Tyus Jones"]
 
-third = data["Age"]                #indexing operator
+# third = data["Age"]                #indexing operator
 
-fourth = data.iloc[0]              #row selection using loc
+# fourth = data.iloc[0]              #row selection using loc
 
-print(first,"\n\n\n",second)
+# print(first,"\n\n\n",second)
 
-print(fourth)
+# print(fourth)
 
  
-import numpy as np
+# import numpy as np
  
-dict = {'First Score':[100, 90, np.nan, 95],
-        'Second Score': [30, 45, 56, np.nan],
-        'Third Score':[np.nan, 40, 80, 98]}
+# dict = {'First Score':[100, 90, np.nan, 95],
+#         'Second Score': [30, 45, 56, np.nan],
+#         'Third Score':[np.nan, 40, 80, 98]}
 
+# df = pd.DataFrame(dict)
+ 
+# print(df.isnull())
+# print()
+
+# print(df.notnull())
+# print()
+
+# print(df.fillna(0)) #any value you put here instead of 0 is replaced by that number in place of missing data 
+
+dict = {'name':["aparna", "pankaj", "sudhir", "Geeku"],
+        'degree': ["MBA", "BCA", "M.Tech", "MBA"],
+        'score':[90, 40, 80, 98]}
+ 
 df = pd.DataFrame(dict)
- 
-print(df.isnull())
-print()
 
-print(df.notnull())
-print()
-
-print(df.fillna(25))
+for i, j in df.iterrows():
+    print(i, j)
+    print()
