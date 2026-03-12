229c19a22f468f5d2222e1b6464b856986e6e60c
Abhinav singh <abhinavsinh014@gmail.com>
2026-03-12 22:11:23 +0530

    Row selecction

--- pandaDataframe.py
+++ pandaDataframe.py
@@ -1,25 +1,31 @@
 import pandas as pd
 
-lst = ['John cena','randy orton','triple h','edge','the rock']
+# lst = ['John cena','randy orton','triple h','edge','the rock']
 
-lf = pd.DataFrame(lst)                 #creating pandas dataframe
-print(lf)
+# lf = pd.DataFrame(lst)                 #creating pandas dataframe
+# print(lf)
 
  
-data = {'Name':['Tom', 'nick', 'krish', 'jack'],          
-        'Age':[20, 21, 19, 18]}                #creating pandas dataframe by using dictionary
+# data = {'Name':['Tom', 'nick', 'krish', 'jack'],          
+#         'Age':[20, 21, 19, 18]}                #creating pandas dataframe by using dictionary
  
-df = pd.DataFrame(data)
+# df = pd.DataFrame(data)
  
-print(df)
+# print(df)
 
-print()
+# print()
 
-deta = {'Name':['Jai', 'Princi', 'Gaurav', 'Anuj'],
-        'Age':[27, 24, 22, 32],
-        'Address':['Delhi', 'Kanpur', 'Allahabad', 'Kannauj'],
-        'Qualification':['Msc', 'MA', 'MCA', 'Phd']}
+# deta = {'Name':['Jai', 'Princi', 'Gaurav', 'Anuj'],
+#         'Age':[27, 24, 22, 32],
+#         'Address':['Delhi', 'Kanpur', 'Allahabad', 'Kannauj'],
+#         'Qualification':['Msc', 'MA', 'MCA', 'Phd']}
  
-gf = pd.DataFrame(deta)
+# gf = pd.DataFrame(deta)               #Column selection
  
-print(gf[['Name', 'Qualification']])
+# print(gf[['Name', 'Qualification']])
+
+data = pd.read_csv("nba.csv",index_col="Name")
+first = data.loc["Avery Bradley"]
+second = data.loc["Tyus Jones"]
+
+print(first,"\n\n\n",second)
