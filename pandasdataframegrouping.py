711f58bf86eccb3f5424000a888e3356bf3e0917
Abhinav singh <abhinavsinh014@gmail.com>
2026-03-15 12:11:31 +0530

    Aggregation

--- pandasdataframegrouping.py
+++ pandasdataframegrouping.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+
 data1 = {'Name':['Jai', 'Anuj', 'Jai', 'Princi',
                  'Gaurav', 'Anuj', 'Princi', 'Abhi'],
         'Age':[27, 24, 22, 32,
@@ -33,5 +35,15 @@ grp = df.groupby('Name')
 print(grp.get_group('Jai'))
 
 grp = df.groupby(['Name','Qualification'])
+
 print(grp.get_group(('Jai','Msc')))
 
+grp1 = df.groupby('Name')
+print(grp1['Age'].aggregate(np.sum))
+
+
+grp2 = df.groupby(['Name','Qualification'])
+print(grp2['Age'].aggregate(np.sum))
+
+grp3 = df.groupby('Name')
+print(grp3['Age'].agg([np.sum, np.mean, np.std]))
