905ee355126fd9b95ba08ef989daf0b4f423baf8
Abhinav singh <abhinavsinh014@gmail.com>
2026-03-17 21:58:06 +0530

    making a bar graph

--- Data_Visualization.py/Data_visualization.py
+++ Data_Visualization.py/Data_visualization.py
@@ -1,13 +1,22 @@
 import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-import seaborn as sns
+# import numpy as np
+# import pandas as pd
+# import seaborn as sns
 
-x = [12,11,14,17,16]
-y = [50,55,62,63,45]
+# x = [12,11,14,17,16]
+# y = [50,55,62,63,45]
 
-plt.plot(x,y)
-plt.title("line chart")
-plt.ylabel('Y-Axis')
-plt.xlabel('X-Axis')
+# plt.plot(x,y)
+# plt.title("line chart")
+# plt.ylabel('Y-Axis')
+# plt.xlabel('X-Axis')
+# plt.show()
+
+x = ['john cena','randy orton','triple h','edge','the rock']
+y = [17,14,14,11,10]
+
+plt.bar(x,y)
+plt.title("world title")
+plt.xlabel("superstars")
+plt.ylabel("championship")
 plt.show()
