b5f6b5dddce8c9514ea14b9e7b84969317d78614
Abhinav singh <abhinavsinh014@gmail.com>
2026-03-17 21:52:27 +0530

    data visualiztion by using plot

---
+++ Data_Visualization.py/Data_visualization.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+x = [12,11,14,17,16]
+y = [50,55,62,63,45]
+
+plt.plot(x,y)
+plt.title("line chart")
+plt.ylabel('Y-Axis')
+plt.xlabel('X-Axis')
+plt.show()
