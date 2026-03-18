08c0b16d4811264d9073cccf4820e749759964de
Abhinav singh <abhinavsinh014@gmail.com>
2026-03-18 12:06:58 +0530

    scatterbgraph

---
+++ Data_Visualization.py/scattergraph.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+x = ['john cena','randy orton','triple h','edge','the rock']
+y = [1700,1254,1104,1100,1998]
+
+plt.scatter(x,y,color='pink')
+plt.title("world title")
+plt.xlabel("superstars")
+plt.ylabel("wins")
+plt.show()
