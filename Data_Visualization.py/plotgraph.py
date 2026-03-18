137301b5622033d59059ffec4d7c2378f8212993
Abhinav singh <abhinavsinh014@gmail.com>
2026-03-18 11:32:55 +0530

    plot graph with pandas and metplotlib

---
+++ Data_Visualization.py/plotgraph.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+marks = {
+    "marks":[36,48,56,78,98,56,45,25,65,45,85,74,96,36,36,96,52]
+}
+
+fg = pd.DataFrame(marks)
+
+plt.plot(fg['marks'],color='red',marker='p',linestyle=':')
+plt.title("line chart")
+plt.ylabel('Y-Axis')
+plt.xlabel('X-Axis')
+plt.show()
