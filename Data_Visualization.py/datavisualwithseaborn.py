e284b93ea9c56387ceb1f4ab18edd0f1131fb461
Abhinav singh <abhinavsinh014@gmail.com>
2026-03-19 21:52:44 +0530

    boxplot

--- Data_Visualization.py/datavisualwithseaborn.py
+++ Data_Visualization.py/datavisualwithseaborn.py
@@ -2,8 +2,8 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-data = {'Name': ['ANSH', 'SAHIL', 'JAYAN', 'ANURAG'], 'Age': [21, 23, 20, 24]}
+data = {'Name': ['ANSH', 'SAHIL', 'JAYAN', 'ANURAG'], 'Age': [21, 23, 20, 64]}
 df = pd.DataFrame(data)
 
-sns.scatterplot(x=df.index, y='Age', data=df)
+sns.boxplot(y='Age', data=df)
 plt.show()
