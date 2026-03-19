7918a1073aa33f7681ce5d959c44aa259427b09d
Abhinav singh <abhinavsinh014@gmail.com>
2026-03-19 21:59:48 +0530

    swarmplot

--- Data_Visualization.py/datavisualwithseaborn.py
+++ Data_Visualization.py/datavisualwithseaborn.py
@@ -5,5 +5,8 @@ import matplotlib.pyplot as plt
 data = {'Name': ['ANSH', 'SAHIL', 'JAYAN', 'ANURAG'], 'Age': [21, 23, 20, 64]}
 df = pd.DataFrame(data)
 
-sns.boxplot(y='Age', data=df)
+# sns.scatterplot(x=df.index,y='Age',data=df)
+# sns.boxplot(y='Age', data=df)
+# sns.violinplot(y='Age', data=df)
+sns.swarmplot(x=df.index, y='Age', data=df,color = 'red')
 plt.show()
