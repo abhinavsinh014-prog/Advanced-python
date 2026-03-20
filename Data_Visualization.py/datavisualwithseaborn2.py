28a6b7018e34cc534daf697f9a4c58d2f8f8fc1d
Abhinav singh <abhinavsinh014@gmail.com>
2026-03-20 21:34:59 +0530

    Adding Titles and Axis Labels

--- Data_Visualization.py/datavisualwithseaborn2.py
+++ Data_Visualization.py/datavisualwithseaborn2.py
@@ -3,11 +3,23 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-iris = load_iris()
-df = pd.DataFrame(iris.data, columns=iris.feature_names)
-df['Species'] = iris.target
-df['Species'] = df['Species'].map({ 0: 'Setosa', 1: 'Versicolor', 2: 'Virginica'})
+# iris = load_iris()
+# df = pd.DataFrame(iris.data, columns=iris.feature_names)
+# df['Species'] = iris.target
+# df['Species'] = df['Species'].map({ 0: 'Setosa', 1: 'Versicolor', 2: 'Virginica'})
 
-sns.kdeplot(data=df[df['Species'] == 'Virginica'], x='sepal length (cm)', fill=True, label='Virginica')
-plt.legend()
+# sns.kdeplot(data=df[df['Species'] == 'Virginica'], x='sepal length (cm)', fill=True, label='Virginica')
+# plt.legend()
+# plt.show()
+
+#How to Customize Seaborn Plots with Python?
+
+#Adding Titles and Axis Labels
+
+iris = sns.load_dataset('iris')
+sns.scatterplot(x='sepal_length', y='sepal_width', data=iris)
+
+plt.title('Sepal Length vs Sepal Width')
+plt.xlabel('Sepal Length (cm)')
+plt.ylabel('Sepal Width (cm)')
 plt.show()
