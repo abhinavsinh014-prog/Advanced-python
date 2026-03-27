2e11ed325ee8a9b74c468c9aaefeb116f0feb1fd
Abhinav singh <abhinavsinh014@gmail.com>
2026-03-27 21:37:51 +0530

    scatterplot with only x data

--- Data_Visualization.py/seabornbasices.py
+++ Data_Visualization.py/seabornbasices.py
@@ -14,12 +14,12 @@ print(penguins["island"].value_counts())
 
 sns.set_style("whitegrid")
 sns.set_context("paper")
-sns.scatterplot(data=penguins,x="flipper_length_mm",y="body_mass_g",hue = "island",palette = "Set1")
-sns.despine(left = True)
-plt.show()
-
-# sns.scatterplot(data=penguins,x="species",y="body_mass_g",hue = "island",style = 'sex',palette = "Set1",alpha = 0.5)
+# sns.scatterplot(data=penguins,x="flipper_length_mm",y="body_mass_g",hue = "island",palette = "Set1")
+# sns.despine(left = True)
 # plt.show()
+
+sns.scatterplot(data=penguins,x="species",y="body_mass_g",hue = "island",style = 'sex',palette = "Set1",alpha = 0.5)
+plt.show()
 
 # sns.stripplot(data=penguins,x="species",y="body_mass_g",hue = "island",dodge=True,jitter=True)
 # plt.show()
