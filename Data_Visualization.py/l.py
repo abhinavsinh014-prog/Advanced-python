97dd00f0b63dd81472e6d06c551827125b2d371b
Abhinav singh <abhinavsinh014@gmail.com>
2026-03-30 13:32:45 +0530

    kde plot

--- Data_Visualization.py/l.py
+++ Data_Visualization.py/l.py
@@ -4,14 +4,14 @@ import matplotlib.pyplot as plt
 
 
 run = {
-    "player" :["Kohli","Rohit","Warner","Smith","Root"],
-    "centuries" : [81,49,49,44,59],
-    "Runs" : [30,10,26,38,41],
-    "100s in 2018" : [54,32,22,12,18],
-    "100s in 2019" : [9,9,8,6,4]
+    "player" :["Kohli","Rohit","Warner"],
+    "centuries" : [81,49,49],
+    "Runs" : [30,10,26],
+    "100s in 2018" : [54,32,22],
+    "100s in 2019" : [9,9,8]
 }
 
 ff  = pd.DataFrame(run)
 
-sns.kdeplot(data=ff,x="centuries",hue="player",palette="pastel",fill =True)
+sns.kdeplot(data=ff,x="centuries",hue='player')
 plt.show()
