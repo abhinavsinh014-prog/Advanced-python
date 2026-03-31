14cf8c54bfa49abec2144978b9ef788275cec6c1
Abhinav singh <abhinavsinh014@gmail.com>
2026-03-31 16:26:49 +0530

    expanding our work

--- Data_Visualization.py/sklearnfile.py
+++ Data_Visualization.py/sklearnfile.py
@@ -1,6 +1,9 @@
 from sklearn.base import BaseEstimator, ClassifierMixin
 import numpy as np
 from sklearn.utils import check_X_y
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_iris
+
 
 class MostFrequentClassClassifier(BaseEstimator, ClassifierMixin):
     def __init__(self):
@@ -22,3 +25,13 @@ class MostFrequentClassClassifier(BaseEstimator, ClassifierMixin):
             raise ValueError("This classifier instance is not fitted yet.")
         
         return np.full(shape=(X.shape[0],), fill_value=self.most_frequent_)
+
+iris = load_iris()
+X, y = iris.data, iris.target
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
+
+  
+classifier = MostFrequentClassClassifier()
+classifier.fit(X_train, y_train)
+
