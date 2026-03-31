98534dae6153c33234dfb7acd6ccdcb27f1b8764
Abhinav singh <abhinavsinh014@gmail.com>
2026-03-31 16:12:29 +0530

    created a def fit

--- Data_Visualization.py/sklearnfile.py
+++ Data_Visualization.py/sklearnfile.py
@@ -6,4 +6,13 @@ class MostFrequentClassClassifier(BaseEstimator, ClassifierMixin):
     def __init__(self):
         self.most_frequent = None
 
-      
+    def fit(self, X, y):
+        
+        X, y = check_X_y(X, y)
+
+        y = np.ravel(y)
+
+        unique_classes, counts = np.unique(y, return_counts=True)
+        self.most_frequent_ = unique_classes[np.argmax(counts)]
+
+        return self    
