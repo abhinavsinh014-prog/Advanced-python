10085ec54c4e2a33027062bd8cb3782a85e78b23
Abhinav singh <abhinavsinh014@gmail.com>
2026-03-31 16:11:29 +0530

    making a class

--- Data_Visualization.py/sklearnfile.py
+++ Data_Visualization.py/sklearnfile.py
@@ -1,3 +1,9 @@
 from sklearn.base import BaseEstimator, ClassifierMixin
 import numpy as np
 from sklearn.utils import check_X_y
+
+class MostFrequentClassClassifier(BaseEstimator, ClassifierMixin):
+    def __init__(self):
+        self.most_frequent = None
+
+      
