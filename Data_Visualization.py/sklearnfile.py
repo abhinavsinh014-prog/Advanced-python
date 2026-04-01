fc906221ef17e848695f551abb087fc8f338184b
Abhinav singh <abhinavsinh014@gmail.com>
2026-04-01 15:45:01 +0530

    scikit learn

--- Data_Visualization.py/sklearnfile.py
+++ Data_Visualization.py/sklearnfile.py
@@ -3,9 +3,11 @@ from sklearn.datasets import fetch_california_housing
 X ,y = fetch_california_housing(return_X_y=True)
 
 from sklearn.linear_model import LinearRegression
-
+import matplotlib.pyplot as plt
 mod = LinearRegression()
 
 mod.fit(X,y)
 
-print(mod.predict(X))
+pred = mod.predict(X)
+
+plt.scatter(pred,y)
