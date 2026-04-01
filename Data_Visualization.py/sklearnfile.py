28f70b3d34e90aa340f6e56f39c36d6105885979
Abhinav singh <abhinavsinh014@gmail.com>
2026-04-01 15:40:05 +0530

    scikit learn

--- Data_Visualization.py/sklearnfile.py
+++ Data_Visualization.py/sklearnfile.py
@@ -1,41 +1,11 @@
-from sklearn.base import BaseEstimator, ClassifierMixin
-import numpy as np
-from sklearn.utils import check_X_y
-from sklearn.model_selection import train_test_split
-from sklearn.datasets import load_iris
+from sklearn.datasets import fetch_california_housing
 
+X ,y = fetch_california_housing(return_X_y=True)
 
-class MostFrequentClassClassifier(BaseEstimator, ClassifierMixin):
-    def __init__(self):
-        self.most_frequent = None
+from sklearn.linear_model import LinearRegression
 
-    def fit(self, X, y):
-        
-        X, y = check_X_y(X, y)
+mod = LinearRegression()
 
-        y = np.ravel(y)
+mod.fit(X,y)
 
-        unique_classes, counts = np.unique(y, return_counts=True)
-        self.most_frequent_ = unique_classes[np.argmax(counts)]
-
-        return self    
-    
-    def predict(self, X):
-        if self.most_frequent_ is None:
-            raise ValueError("This classifier instance is not fitted yet.")
-        
-        return np.full(shape=(X.shape[0],), fill_value=self.most_frequent_)
-
-iris = load_iris()
-X, y = iris.data, iris.target
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
-
-  
-classifier = MostFrequentClassClassifier()
-classifier.fit(X_train, y_train)
-
-predictions = classifier.predict(X_test)
-print(f"Predicted class for all test instances: {predictions[0]}")
-
-print(classifier.most_frequent_)
+print(mod.predict(X))
