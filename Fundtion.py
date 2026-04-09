16f80688465ec043b6dc8eed2b71c75a1d635bd1
Abhinav singh <abhinavsinh014@gmail.com>
2026-04-09 14:42:38 +0530

    giving more data

--- Fundtion.py
+++ Fundtion.py
@@ -1,6 +1,7 @@
-def intro():
+def intro() -> str:
     return "Welcome to the Python programming world!"
 
 
 msd = intro()
 print(msd)
+print(msd+' This is a simple function that returns a welcome message.')
