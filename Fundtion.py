30bac1831af3dd05e00deb48586b89138e6b3f1c
Abhinav singh <abhinavsinh014@gmail.com>
2026-04-09 14:47:46 +0530

    created a new function

--- Fundtion.py
+++ Fundtion.py
@@ -5,3 +5,13 @@ def intro() -> str:
 msd = intro()
 print(msd)
 print(msd+' This is a simple function that returns a welcome message.')
+
+def odd_even(list1):
+    odd = []
+    even = []
+    for num in list1:
+        if num % 2 == 0:
+            even.append(num)
+        else:
+            odd.append(num)
+    return odd, even
