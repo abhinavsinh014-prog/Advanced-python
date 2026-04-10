2289100a3cfd844f665252f8d3822c407ff24d8d
Abhinav singh <abhinavsinh014@gmail.com>
2026-04-10 20:26:19 +0530

    creating a new function

--- Fundtion.py
+++ Fundtion.py
@@ -1,22 +1,25 @@
-def intro() -> str:
-    return "Welcome to the Python programming world!"
+# def intro() -> str:
+#     return "Welcome to the Python programming world!"
 
 
-msd = intro()
-print(msd)
-print(msd+' This is a simple function that returns a welcome message.')
+# msd = intro()
+# print(msd)
+# print(msd+' This is a simple function that returns a welcome message.')
 
-def odd_even(list1):
-    odd = []
-    even = []
-    for num in list1:
-        if num % 2 == 0:
-            even.append(num)
-        else:
-            odd.append(num)
-    return odd, even
+# def odd_even(list1):
+#     odd = []
+#     even = []
+#     for num in list1:
+#         if num % 2 == 0:
+#             even.append(num)
+#         else:
+#             odd.append(num)
+#     return odd, even
 
 
-num1, num2 = odd_even([31,45,18,17,7,10,56,4,36,89,25,12,77,333])
-print("Odd numbers:", num1)
-print("Even numbers:", num2)
+# num1, num2 = odd_even([31,45,18,17,7,10,56,4,36,89,25,12,77,333])
+# print("Odd numbers:", num1)
+# print("Even numbers:", num2)
+
+def van(name,wins):
+    return f"{name} has won {wins} times championship."
