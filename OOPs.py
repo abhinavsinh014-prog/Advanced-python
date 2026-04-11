4d25750b5ac7c838dcf060fd4179f2867a307ce9
Abhinav singh <abhinavsinh014@gmail.com>
2026-04-11 20:50:24 +0530

    added new file

--- OOPs.py
+++ OOPs.py
@@ -4,6 +4,10 @@ class car:
         self.tyres=tyres
         self.engine=engine
 
+    def drive(self):
+        print("Car have a {} engine".format(self.engine))
+
 car1 = car(4,4,"disel")
-print("Car 1 has {} windows, {} tyres and {} engine.".format(car1.windows,car1.tyres,car1.engine))
-print(f"Car 1 has {car1.windows} windows, {car1.tyres} tyres and {car1.engine} engine.")
+print("Car has {} windows, {} tyres and {} engine.".format(car1.windows,car1.tyres,car1.engine))
+print(f"Car has {car1.windows} windows, {car1.tyres} tyres and {car1.engine} engine.")
+car1.drive()
