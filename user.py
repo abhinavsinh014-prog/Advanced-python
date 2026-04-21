7153469405ea8b20e3ae3b558e47fb27a1e59ad9
Abhinav singh <abhinavsinh014@gmail.com>
2026-04-21 22:17:43 +0530

    added

---
+++ user.py
@@ -0,0 +1,8 @@
+data = {
+    "John cena": 1765,
+    "The rock": 2810,
+    "Iron man": 3980,
+    "Captain America": 2155
+}
+input_name = input("Enter the name of the wrestler: ")
+if input_name in data:
