62c856d2114e8bf1d6ae6bb7e9c6b7d82cd51d7b
Abhinav singh <abhinavsinh014@gmail.com>
2026-04-22 22:08:12 +0530

    added

--- fhats.py
+++ fhats.py
@@ -1,5 +1,5 @@
 a = [[5,6,9],[2,3,6]]
-b = [ [4,5,6],[2,3,7]]
+m = [ [4,5,6],[2,3,7]]
 
-dd = [[a[i][j] + b[i][j] for j in range(len(a[0]))] for i in range(len(a))]
+dd = [[a[i][j] + m[i][j] for j in range(len(a[0]))] for i in range(len(a))]
 print("Aura addition:\n",dd)
