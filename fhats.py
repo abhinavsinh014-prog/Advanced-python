e07c692e721cb2250d955ea0e213bf6b2c8e0186
Abhinav singh <abhinavsinh014@gmail.com>
2026-04-25 20:11:53 +0530

    added

--- fhats.py
+++ fhats.py
@@ -9,7 +9,4 @@
 # print(b+135*3/int(n))
 lab = ['p','o','w','e','r','r','a','n','g','e','r']
 for i in range(len(lab)):
-    if i%2==0:
-        print(lab[i],end="")
-    else:        
-        print(lab[i].upper(),end="")
+    print(lab[i].upper(),end="")
