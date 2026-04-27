098dde534777e49a5eb90c1bc5899dfd73b58275
Abhinav singh <abhinavsinh014@gmail.com>
2026-04-27 22:45:42 +0530

    added

--- rpu.py
+++ rpu.py
@@ -1,5 +1,6 @@
 quantity = {"apple": 1, "banana": 2, "orange": 3, "grape": 4, "melon": 5}
 price = {"apple": 1.5, "banana": 0.5, "orange": 0.75, "grape": 2.0, "melon": 3.0}
+expiry = {"apple": "2024-07-01", "banana": "2024-06-30", "orange": "2024-07-15", "grape": "2024-07-10", "melon": "2024-07-20"}
 def add_fruits(fruit_dict):
     total = 0
     for fruit, quantity in fruit_dict.items():
