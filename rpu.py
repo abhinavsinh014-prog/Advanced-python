eb9423eb78a525bb4cdd4e11e748670025de7d4e
Abhinav singh <abhinavsinh014@gmail.com>
2026-04-27 22:41:13 +0530

    added

--- rpu.py
+++ rpu.py
@@ -4,3 +4,7 @@ def add_fruits(fruit_dict):
     for fruit, quantity in fruit_dict.items():
         total += quantity
     return total
+
+if __name__ == "__main__":
+    total_fruits = add_fruits(add)
+    print(f"Total fruits: {total_fruits}")
