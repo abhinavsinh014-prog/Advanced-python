b4ebe18f8e4f5fa80a37dab901d964f5e7d247a6
Abhinav singh <abhinavsinh014@gmail.com>
2026-04-29 18:15:37 +0530

    added

--- freewill.py
+++ freewill.py
@@ -5,3 +5,12 @@ for i in range(len(arr)-1, -1, -1):
     reversed_arr.append(arr[i])
 
 print("Reversed list:", reversed_arr)
+
+arr = [1, 2, 3, 2, 4, 5, 1]
+
+duplicates = []
+for num in arr:
+    if arr.count(num) > 1 and num not in duplicates:
+        duplicates.append(num)
+
+print("Duplicates:", duplicates)
