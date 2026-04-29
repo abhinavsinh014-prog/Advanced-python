6005bf08546c86ca52b0aae22efee84c6a3062d8
Abhinav singh <abhinavsinh014@gmail.com>
2026-04-29 18:17:52 +0530

    added

--- freewill.py
+++ freewill.py
@@ -40,3 +40,15 @@ for num in arr:
         freq[num] = 1
 
 print("Frequency:", freq)
+
+num = int(input("Enter number: "))
+
+if num > 1:
+    for i in range(2, num):
+        if num % i == 0:
+            print("Not Prime")
+            break
+    else:
+        print("Prime Number")
+else:
+    print("Not Prime")
