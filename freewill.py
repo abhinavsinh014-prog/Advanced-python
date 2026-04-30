53195bc93228ebb8fba6af565298d0e9aa6868e0
Abhinav singh <abhinavsinh014@gmail.com>
2026-04-30 22:27:07 +0530

    added

--- freewill.py
+++ freewill.py
@@ -64,3 +64,11 @@ computer = random.choice(choices)
 
 print("Computer:", computer)
 
+if user == computer:
+    print("Draw")
+elif (user == "rock" and computer == "scissors") or \
+     (user == "paper" and computer == "rock") or \
+     (user == "scissors" and computer == "paper"):
+    print("You Win")
+else:
+    print("You Lose")
