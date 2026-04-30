1053180d7c55a81cb1ea5b523dd59e22d81a50d0
Abhinav singh <abhinavsinh014@gmail.com>
2026-04-30 22:25:01 +0530

    added

--- freewill.py
+++ freewill.py
@@ -1,54 +1,59 @@
-arr = [1, 2, 3, 4, 5]
+# arr = [1, 2, 3, 4, 5]
 
-reversed_arr = []
-for i in range(len(arr)-1, -1, -1):
-    reversed_arr.append(arr[i])
+# reversed_arr = []
+# for i in range(len(arr)-1, -1, -1):
+#     reversed_arr.append(arr[i])
 
-print("Reversed list:", reversed_arr)
+# print("Reversed list:", reversed_arr)
 
-arr = [1, 2, 3, 2, 4, 5, 1]
+# arr = [1, 2, 3, 2, 4, 5, 1]
 
-duplicates = []
-for num in arr:
-    if arr.count(num) > 1 and num not in duplicates:
-        duplicates.append(num)
+# duplicates = []
+# for num in arr:
+#     if arr.count(num) > 1 and num not in duplicates:
+#         duplicates.append(num)
 
-print("Duplicates:", duplicates)
+# print("Duplicates:", duplicates)
 
-# calculator.py
-# a = float(input("Enter first number: "))
-# b = float(input("Enter second number: "))
+# # calculator.py
+# # a = float(input("Enter first number: "))
+# # b = float(input("Enter second number: "))
 
-# print("Addition:", a + b)
-# print("Subtraction:", a - b)
-# print("Multiplication:", a * b)
+# # print("Addition:", a + b)
+# # print("Subtraction:", a - b)
+# # print("Multiplication:", a * b)
 
-# if b != 0:
-#     print("Division:", a / b)
+# # if b != 0:
+# #     print("Division:", a / b)
+# # else:
+# #     print("Division by zero not allowed")
+
+# # frequency_count.py
+# arr = [1, 2, 2, 3, 3, 3, 4]
+
+# freq = {}
+
+# for num in arr:
+#     if num in freq:
+#         freq[num] += 1
+#     else:
+#         freq[num] = 1
+
+# print("Frequency:", freq)
+
+# num = int(input("Enter number: "))
+
+# if num > 1:
+#     for i in range(2, num):
+#         if num % i == 0:
+#             print("It's not a Prime")
+#             break
+#     else:
+#         print("It's a Prime Number")
 # else:
-#     print("Division by zero not allowed")
+#     print("It's not a Prime")
 
-# frequency_count.py
-arr = [1, 2, 2, 3, 3, 3, 4]
 
-freq = {}
+# rock_paper_scissors.py
+import random
 
-for num in arr:
-    if num in freq:
-        freq[num] += 1
-    else:
-        freq[num] = 1
-
-print("Frequency:", freq)
-
-num = int(input("Enter number: "))
-
-if num > 1:
-    for i in range(2, num):
-        if num % i == 0:
-            print("It's not a Prime")
-            break
-    else:
-        print("It's a Prime Number")
-else:
-    print("It's not a Prime")
