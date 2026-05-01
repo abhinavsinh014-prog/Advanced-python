32c1e94d8bbe594a3d909ba424104d53d6a86a3a
Abhinav singh <abhinavsinh014@gmail.com>
2026-05-01 23:17:06 +0530

    added

--- Data_Visualization.py/seabornbasices.py
+++ Data_Visualization.py/seabornbasices.py
@@ -40,8 +40,8 @@ sns.set_context("paper")
 # sns.jointplot(data=penguins,x="body_mass_g",y='flipper_length_mm',hue='sex',kind='scatter')
 # plt.show()
 
-# sns.barplot(data=penguins,x="species",y='body_mass_g',hue='sex',palette=['pink','blue'],estimator=np.sum)
-# plt.show()
+sns.barplot(data=penguins,x="species",y='body_mass_g',hue='sex',palette=['pink','blue'],estimator=np.sum)
+plt.show()
 
 sns.countplot(data=penguins,x="species",hue='island',palette='Set1')
 plt.show()
