f006f6eeed5038d668c8dc00402a75ce875b61a5
Abhinav singh <abhinavsinh014@gmail.com>
2026-05-03 22:33:03 +0530

    giving some data

---
+++ ATMsystem.py
@@ -0,0 +1,10 @@
+bank = {
+    "aditi": {"balance": 95454, "pin": 1234},
+    "naman": {"balance": 89878, "pin": 5678},
+    "randy": {"balance": 65255, "pin": 9012},
+    "john": {"balance": 31218, "pin": 3456},
+    "pandat": {"balance": 31982, "pin": 7890},
+    "gadariya": {"balance": 56512, "pin": 2345},
+    "mohit": {"balance": 28879, "pin": 4567}
+}
+
