b881bc26ca97333166e2c091cbe4b57080c4a5cf
Abhinav singh <abhinavsinh014@gmail.com>
2026-05-05 22:21:48 +0530

    added

---
+++ study.py
@@ -0,0 +1,8 @@
+study = {
+    "subject": "Mathematics",
+    "topics": ["Algebra", "Calculus", "Geometry"],
+    "exams": {
+        "midterm": "2024-10-15",
+        "final": "2024-12-20"
+    }
+}
