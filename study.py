40696eeb9de845ef650ecdffe76e37509f118edf
Abhinav singh <abhinavsinh014@gmail.com>
2026-05-05 22:25:54 +0530

    added

--- study.py
+++ study.py
@@ -3,3 +3,7 @@ study = {
     "topics": [5,5,5,5,5],
     "exams DATE": ["2024-07-01","2024-07-02","2024-07-03","2024-07-04","2024-07-05"]
 }
+
+import pandas as pd
+df = pd.DataFrame(study)
+print(df)
